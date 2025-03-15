68a12de6a08353a4e82f577e61a55aacd3bb5c4f
Wata-dev0 <watashi2948@gmail.com>
2025-03-15 16:58:04 -0300

    clima

---
+++ projetos/websites/clima/script.js
@@ -0,0 +1,14 @@
+
+
+function ftoc() {
+
+    const tmp = document.getElementById("temphere").textContent; 
+    const value = parseFloat(tmp);
+
+    document.getElementById("temphere").textContent = tmp.includes("C") 
+        ? `${((value * 9/5) + 32).toFixed(1)}°F` 
+        : `${((value - 32) * 5/9).toFixed(1)}°C`;
+}
+
+
+
